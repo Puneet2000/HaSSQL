a0c5cd3562e6c9bbd1d4062a379f1aaad1c4d69e
Puneet Mangla <cs17btech11029@iith.ac.in>
2019-03-27 21:41:10 +0530

    Basic Parsers (#3)
    Fix #2 Operators table, parenthesis, integer parser
    Author: Puneet2000

---
+++ src/Funcs.hs
@@ -0,0 +1,28 @@
+module Funcs
+(regularParse ,
+parseWithEof ,
+parseWithLeftOver ,
+parseWithWSEof ) where 
+import Text.Parsec (ParseError)
+import Text.Parsec.String (Parser)
+import Text.Parsec (parse)
+import Text.ParserCombinators.Parsec.Char (oneOf)
+import Text.ParserCombinators.Parsec.Combinator (eof,manyTill,anyToken)
+import Control.Applicative ((<$>), (<*>), (<*), (*>), many)
+import Control.Monad (void)
+
+regularParse :: Parser a -> String -> Either ParseError a
+regularParse p = parse p ""
+
+parseWithEof :: Parser a -> String -> Either ParseError a
+parseWithEof p = parse (p <* eof) ""
+
+parseWithLeftOver :: Parser a -> String -> Either ParseError (a,String)
+parseWithLeftOver p = parse ((,) <$> p <*> leftOver) ""
+  where leftOver = manyTill anyToken eof
+
+parseWithWSEof :: Parser a -> String -> Either ParseError a
+parseWithWSEof p = parseWithEof (whiteSpace *> p)
+  where whiteSpace = void $ many $ oneOf " \n\t"
+
+
